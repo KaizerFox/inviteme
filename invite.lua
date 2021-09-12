9190d7fdb1fa4df51f05aab929da172cb61a5a67
KaizerFox <29178963+KaizerFox@users.noreply.github.com>
2021-09-12 18:31:34 -0400

    Create invite.lua

---
+++ invite.lua
@@ -0,0 +1,32 @@
+if syn then
+		syn.request({
+			Url = "http://127.0.0.1:6463/rpc?v=1",
+			Method = "POST",
+			Headers = {
+					["Content-Type"] = "application/json";
+					["Origin"] = "https://discord.com";
+			},
+			Body = game:GetService("HttpService"):JSONEncode({
+				["cmd"] = "INVITE_BROWSER";
+				["args"] = { ["code"] = "EnXK2SVBCu" }; -- put invite code 
+				["nonce"] = game:GetService("HttpService"):GenerateGUID(false)
+			})
+		})
+
+		else 
+
+		pcall(function()
+			http.request({
+			Url = "http://127.0.0.1:6463/rpc?v=1",
+			Method = "POST",
+			Headers = {
+					["Content-Type"] = "application/json";
+					["Origin"] = "https://discord.com";
+			},
+			Body = game:GetService("HttpService"):JSONEncode({
+				["cmd"] = "INVITE_BROWSER";
+				["args"] = { ["code"] = "EnXK2SVBCu" }; -- put invite code 
+				["nonce"] = game:GetService("HttpService"):GenerateGUID(false)
+			})
+		})
+		end)
